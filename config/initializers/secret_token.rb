0da33fa261b730176c6359b639ca75dc8d7ae497
yakkuru201 <tonamimagic@gmail.com>
2014-02-01 13:54:12 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Keijiban2::Application.config.secret_key_base = '19369a5f6809d239d9033b9c357e672282b39963855ff24966acedd46f98a2f1b53bf7ce8060131b4b7d745710cdddde7c35407cc716b492d6d2eecad50b8051'
