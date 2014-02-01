5f873e4f2e31348d6048868ef0fd49fc527723ac
yakkuru201 <tonamimagic@gmail.com>
2014-02-01 17:45:13 +0900

    generate controller comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Keijiban2::Application.routes.draw do
+  get "comments/create"
+  get "comments/edit"
+  get "comments/update"
+  get "comments/destroy"
   resources :boards
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
