048110534a43977167fe2ba4b23713a23458561b
yakkuru201 <tonamimagic@gmail.com>
2014-02-01 19:32:18 +0900

    define comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
   def create
+    @board = Board.find(params[:board_id])
+    @comment = @board.comments.build params_comment
+    @comment.save
+    redirect_to board_path(@board)
   end
 
   def edit
@@ -9,5 +13,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def params_comment
+    params.require(:comment).permit(:content)
   end
 end
